ecebd81b654c5d52630d434c8810678fc0b40010
Eldon Williams <eldon.ew.com@gmail.com>
2023-04-20 18:11:32 -0400

    added cli and update documentation

--- challenge-one/index.js
+++ challenge-one/index.js
@@ -44,6 +44,8 @@ function encrypt(object) {
 
 /**
  * Decrypts encrypted string or number to string
+ * O(n), but n is stuck at 4. Other constants play a big factor to time complexity
+ * Memory complexity is much more important too
  * @param {string | number} object encrypted object to decrypt 
  * @returns decrypted string
  */
@@ -53,15 +55,71 @@ function decrypt(object) {
     object = object.padStart(4, "0"); // pad using same trick as earlier
     let collection = new Array(4); // make pre-sized array to avoid dynamic sizing
     object.split('').forEach((char, i) => { // run through each, using same map, and using the
-                                            // *negative* modulo
+        // *negative* modulo
         collection[encryptionMapLookup[i]] = negativeCompleteModulo(parseInt(char) - 7, 10);
     });
     return collection.join('');
 }
 
-console.log("Integer 1234 is encrypted to 0189: ", encrypt(1234));
-console.log("Integer 0189 is decrypted to 1234: ", decrypt(189));
-console.log("Integer 9562 is encrypted to 3962: ", encrypt("9562"));
-console.log("Integer 3962 is decrypted to 9562: ", decrypt("3962"));
+// uncomment to showcase
+// console.log("Integer 1234 is encrypted to 0189: ", encrypt(1234));
+// console.log("Integer 0189 is decrypted to 1234: ", decrypt(189));
+// console.log("Integer 9562 is encrypted to 3962: ", encrypt("9562"));
+// console.log("Integer 3962 is decrypted to 9562: ", decrypt("3962"));
 
-module.exports = { encrypt, isInt };
+// uncomment to use as module
+// module.exports = { encrypt, decrypt };
+
+// uncomment to use as cli
+
+const path = require('path');
+const { argv, exit } = require('process');
+const { readFileSync, writeFileSync } = require('fs');
+
+const opt = argv[2];
+const workingFile = argv[3];
+const toProcess = argv.slice(4);
+
+const isEncrypting = opt == "-e" || opt == "-encrypt";
+
+// Do help already
+if (opt === "-h") {
+    console.log(`
+-e | -encrypt <saveFile> [...numbers]
+        Encrypt from numbers from stdin, save to file
+-d | -decrypt [loadFile?] [...numbers?]
+        Decrypt from a file or from stdin.
+        Writes result to stdout.
+-h
+        Write this message to console.
+        `);
+    exit(0);
+}
+
+// if (opt !== "-decrypt" && opt !== "-d" && !isEncrypting) // invalid opt
+//     exit(1);
+
+if (!isNaN(parseInt(workingFile))) {
+    toProcess.push(workingFile);
+    workingFile = null;
+    if (isEncrypting) exit(1);
+}
+
+if (isEncrypting) {
+    let collection = new Array(toProcess.length);
+    toProcess.forEach((num, i) => {
+        let result = encrypt(num);
+        collection[i] = result;
+    });
+    writeFileSync(path.join(process.cwd(), workingFile), collection.join('\n'));
+} else {
+    if (workingFile !== null) {
+        let raw = readFileSync(path.join(process.cwd(), workingFile)).toString('utf-8');
+        toProcess.push(...raw.split('\n'));
+    }
+    let collection = new Array(toProcess.length);
+    toProcess.forEach((num, i) => {
+        collection[i] = decrypt(num);
+    });
+    writeFileSync(path.join(process.cwd(), workingFile), collection.join('\n'));
+}
